c15596c4187ef0fc642409196a5de967905cea84
Brian <briantechdom@gmail.com>
2022-02-27 14:53:43 -0800

    Added missing wp_head() php tag in header.php

--- header.php
+++ header.php
@@ -29,6 +29,7 @@
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet">
         
+    <?php wp_head(); ?>    
     </head>
 
     <body class="blog">
